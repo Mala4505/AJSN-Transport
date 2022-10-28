070c9e002f4082144373a678de3eecea4fc4ed93
Mala4505 <aliasgerzmala@gmail.com>
2022-10-28 15:23:23 +0300

    Request Post Completed

--- user/views.py
+++ user/views.py
@@ -1,17 +1,11 @@
-from multiprocessing import context
 from django.shortcuts import redirect, render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
 
-
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
 
 from django.contrib.auth.decorators import login_required
 
 from user.models import *
-from user.serializers import BookingSerializer
 from user.forms import *
 
 
@@ -32,17 +26,3 @@ def home(request):
         options = CB_Option_Master.objects.all()
         context = {'options':options}
         return render(request, 'home.html', context)
-    
-    # else:
-    #     return render(request, 'home.html')
-
-
-# @api_view(['POST'])
-# def createBooking(request):
-#     serializer = BookingSerializer(data=request.data)
-
-#     if serializer.is_valid():
-#         serializer.save()
-
-#     return Response(serializer.data)
-
